dcfe131d09b1b7f04f6ed54a6cfcdc42d2767133
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-04-01 02:27:38 +0200

    fix

--- app/informazioni/page.tsx
+++ app/informazioni/page.tsx
@@ -79,7 +79,7 @@ export default function InformazioniPage() {
           {/* BOX CTA MENÙ */}
           <div className="relative w-full max-w-4xl mx-auto h-[400px] rounded-sm overflow-hidden shadow-2xl group">
             <Image 
-              src="/gramigna.jpeg" 
+              src="/salumi5.jpg" 
               alt="Guarda il menù"
               fill
               className="object-cover transition-transform duration-700 group-hover:scale-105"
