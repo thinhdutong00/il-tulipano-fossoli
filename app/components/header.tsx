8b6818442e365cfbfd083c8f2f9c6341a49e54eb
Du Tong Thinh <dutongthinh@mac.lan>
2026-06-05 14:47:03 +0100

    fix

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -180,7 +180,7 @@ export default function Header() {
             }}>
               Prenota un tavolo
             </Link>
-            <a href="tel:0598678400" style={{ 
+            <a href="tel:+390598678400" style={{ 
               border: `1px solid ${panna}`, 
               color: panna, 
               textAlign: 'center', 
